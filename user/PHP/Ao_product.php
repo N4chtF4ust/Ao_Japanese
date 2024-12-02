ebd07a4708c9841e305d7872c9396bc921ca8130
N4chtF4ust <ronguinto57@gmail.com>
2024-12-02 21:17:36 +0800

    Add new database

--- user/PHP/Ao_product.php
+++ user/PHP/Ao_product.php
@@ -120,8 +120,25 @@ for ($i=1; $i <=10 ; $i++) {
         </div>
 
         <div class="button_wrapper">
-           <button id="cart_buy">Buy</button>
-           <button id="cart_close" onclick="toggleCart(document.getElementById('cart_wrapper'))">Close</button>
+
+           <button id="cart_buy">
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="50%" height="50%" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+            </svg> 
+
+            <h4> Buy </h4> 
+
+          </button>
+           <button id="cart_close" onclick="toggleCart(document.getElementById('cart_wrapper'))">
+
+           <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="50%" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
+           <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+           <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+           
+          </svg>
+           <h4>Close</h4>
+          </button>
 
         </div>
            
